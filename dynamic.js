eba345968f6d17f32ce0a8342068183b977b5c21
Aaron Kammula <aaron.kammula@gmail.com>
2018-01-13 00:09:39 -0500

    Introduction Page
    Here is the starting page to my other pages. Although not as colourful, I am still improving on this during the next few months of 2018.

---
+++ dynamic.js
@@ -0,0 +1,10 @@
+//This is the part where I make the sidebar menu function 
+
+//Toggle between hiding and showing the dropdown contents (takes all <a href> elements, and displays them)
+
+function openButton() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+function closeButton() {
+  document.getElementById("mySidebar").style.display = "none";
+}
