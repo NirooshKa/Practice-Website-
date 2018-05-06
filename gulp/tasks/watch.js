d3534600641c56edfe8f0157f47de21d71828be5
AaronK <aaron.kammula@gmail.com>
2018-05-06 02:19:16 -0400

    New Post CSS Features
    Using and obtaining SASS libraries with the help of NPM and gulp.js

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+//Basically declaring a class
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false, //Gets rid of annoying change notification
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+});
+
+//Square bracket => styles must be completed before running this task
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+//Basically like # include, but for gulp.js
+require('./gulp/tasks/styles'); //no need for .js file description
+require('./gulp/tasks/watch');
+
+
+
+
+
+
+
+
+
+
+
